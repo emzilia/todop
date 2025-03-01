dc51cd4dee14c42a17fa3529823c6fd40f050b6f
emzilia <jack.star509@gmail.com>
2025-03-01 11:18:52 -0500

    updated project format

---
+++ src/todop/__main__.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    from .todop.cli import app
+    app.process_args()
---
+++ src/todop/cli.py
@@ -0,0 +1,6 @@
+from .todop import NoteList
+
+app = NoteList()
+
+if __name__ == "__main__":
+    app.process_args()
