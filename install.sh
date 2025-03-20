e92c87a4b2474ad71394bf3d9d18686a8cf0161f
emzilia <em@zilia.dev>
2025-03-20 17:27:26 -0400

    removed redundant func, silly in hindsight

--- install.sh
+++ install.sh
@@ -3,26 +3,17 @@
 SCRIPT='todop'
 SCRIPTPATH="$HOME/.local/bin/tdp"
 
-# Checks whether script has been copied over.
-file_exists () {
-	if [ -f "$SCRIPTPATH" ]; then
-		echo 1
-	else
-		echo 0
-	fi
-}
-
 # If file exists, remove it, if it doesn't, copy it over.
-if [ "$(file_exists)" -eq 1 ]; then
+if [ -f "$SCRIPTPATH" ]; then
 	echo "Uninstalling todop script"
 	rm "$SCRIPTPATH"
-	if [ "$(file_exists)" -eq 0 ]; then
+	if [ ! -f "$SCRIPTPATH" ]; then
 		echo "Script successfully uninstalled!"
 	fi
 else
 	echo "Installing todop script"
 	cp "$SCRIPT" "$SCRIPTPATH"
-	if [ "$(file_exists)" -eq 1 ]; then
+	if [ -f "$SCRIPTPATH" ]; then
 		echo "Script successfully installed!"
 	fi
 fi
